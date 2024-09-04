5cd5f116ac18954cca3f0629e99c281598103dae
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-04 18:21:34 +0000

    Print all posibles combinations of two digits

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for a in range(9):
     for b in range(a + 1, 10):
         if a == 8 and 9 == 9:
-            print("{}{}".format(a,b))
+            print("{}{}".format(a, b))
         elif a != b:
             print("{}{}".format(a, b), end=', ')
