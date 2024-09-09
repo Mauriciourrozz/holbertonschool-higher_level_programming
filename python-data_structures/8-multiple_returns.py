577e20f603c9878a32d9e95ff2c5bbd80e057015
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-09 23:22:19 +0000

    Returns a tuple

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if not sentence:
-        return None
-    if not sentence[0]:
-        return None
+        sentence[0] = None
     length = len(sentence)
     first = sentence[0]
     return length, first
