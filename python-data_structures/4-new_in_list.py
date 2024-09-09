374a58c1b44029c1c0f273bf73e4d874edd65ac7
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-09 14:24:30 -0300

    Replaces an element

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     nueva_lista = list(my_list)  # list() me duplica la lista
     if idx < 0 and idx >= len(my_list):
-        return my_list
+        return nueva_lista
     nueva_lista[idx] = element
     return nueva_lista
