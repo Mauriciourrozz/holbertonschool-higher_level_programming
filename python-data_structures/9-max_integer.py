fe02a379763dd8fa2afa778e4d306ccd0d337dce
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-10 09:56:51 -0300

    Find the biggest integer

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     for num in my_list:
         maximo = my_list[0]
-        for i in range(1, len(my_list)):
+        for i in range(1, len(my_list) + 1):
             if my_list[i] > maximo:
                 maximo = my_list[i]
             return maximo
