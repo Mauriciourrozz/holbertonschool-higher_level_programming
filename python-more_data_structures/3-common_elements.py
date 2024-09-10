534b41f113e6c5e90689d09ff62ca9f8ae7ec0fc
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-10 16:11:21 -0300

    Returns a set of common elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
     for i in set_1:
-        for i in set_2:
-            if i in set_1 and i in set_2:
-                return i
-            if i not in set_1 or i not in set_2:
-                return None
+        if i in set_2:
+            return i
