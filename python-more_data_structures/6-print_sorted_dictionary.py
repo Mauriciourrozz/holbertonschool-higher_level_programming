05dd52ef4787225e0ba96e8eabab157b5412398f
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-11 11:36:00 -0300

    Prints a dictionary

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     order = sorted(a_dictionary)
     for element in order:
-        print(f"{element} : {a_dictionary[element]}")
+        print(f"{element}{a_dictionary[element]}")
