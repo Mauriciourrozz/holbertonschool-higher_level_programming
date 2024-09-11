8027b22716e948013861bac81b2bf603ba9fcafd
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-11 09:49:50 -0300

    Returns a set of common elements

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    vacio = []
-    for i in set_1:
-        for i in set_2:
-            if i in set_1 and i in set_2:
-                vacio.append(i)
-    return vacio
+    return set_1 & set_2
