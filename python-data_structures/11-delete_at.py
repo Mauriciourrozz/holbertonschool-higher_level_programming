67a5889c72dd47de82dfb14dc963fd00f81f7046
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-12 11:03:31 -0300

    Delete en element of a list

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    """ idx -> 5 | my_list -> [1,2,3,4,5] 5 """
     if idx < 0 or idx >= len(my_list):
         return my_list
     del my_list[idx]
