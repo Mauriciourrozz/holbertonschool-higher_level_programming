87456073afe605ddebd71921f6d69a1b909fe3e2
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-14 21:11:35 +0000

    Print the first x element of a list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
     try:
         for i in range(x):
             if isinstance(i, int):
-                print("{}".format(i), end="")
+                print("{:d}".format(i), end="")
                 cont += 1
         print()
     except IndexError:
