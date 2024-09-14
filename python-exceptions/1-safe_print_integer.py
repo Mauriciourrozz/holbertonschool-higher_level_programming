756acdb50804beb8c8972e4893aff4a2c8f7fed7
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-14 20:23:56 +0000

    Print an integer

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,7 @@ def safe_print_integer(value):
         if isinstance(value, int):
             print("{:d}".format(value))
             return True
-    except ValueError:
-        if not isinstance(value, int):
+        else:
             return False
+    except ValueError:
+        return False
