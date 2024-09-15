3bfd23ee9c28e74c850d45f3c31103944c41e6ac
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-15 00:05:42 +0000

    Print the first x element of a list

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -14,5 +14,6 @@ def safe_print_list_integers(my_list=[], x=0):
                         count += 1
         print()
     except TypeError:
-        print()
+        print("f")
+    print()
     return count
