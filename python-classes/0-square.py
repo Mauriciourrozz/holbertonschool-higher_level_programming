88a25c6e9c02babfc624085bc890ca11dabe91e0
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-16 13:28:07 -0300

    Write an empty class

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,5 +2,8 @@
 class Square:
     """
     The class 'Square' represent a square
+
+    This class is empty in this version, but can be extended
+    to include attributes and methods related to a square.
     """
     pass
