87acea3db8bab4d6d2ba6bb56a91f964ef6d9a7b
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-17 12:39:35 -0300

    Write a class that defines a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -9,8 +9,8 @@ class Square:
     This class represent a square
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         return self.__size * self.__size
