2a93f237665c4f8c2fe33040b0ba4fd08f690cbd
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-17 12:17:46 -0300

    Write a class that defines a square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -44,6 +44,6 @@ class Square:
     def position(self, value):
         if not isinstance(value, tuple) and len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
-        if not all(isinstance(num, int) and num > 0 for num in value):
+        if not all([isinstance(num, int) and num > 0 for num in value]):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
