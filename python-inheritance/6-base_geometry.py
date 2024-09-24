fdcc5b73e52e5bba57d752c6b54e38cc9fef9da0
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-24 11:30:49 -0300

    Class BaseGeometry

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -9,9 +9,5 @@ class BaseGeometry:
     This class represent a Base Geometry
 
     """
-    def __init__(self, alto=0, ancho=0):
-        self.alto = alto
-        self.ancho = ancho
-
     def area(self):
-            raise Exception("area() is not implemented")
+        raise Exception("area() is not implemented")
