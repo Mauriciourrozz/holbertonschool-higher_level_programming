0b1b644470b0c20b6f07f5fa13e7470270afe107
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-25 16:18:09 -0300

    Write a class MyList

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+This file contains a class
+"""
+
+
 class MyList(list):
+    """
+    This class represent a list
+    """
     def print_sorted(self):
         lista_ordenada = sorted(self)
         print(lista_ordenada)
