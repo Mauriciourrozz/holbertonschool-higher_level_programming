d427a41042ca9d7e306a7a88e5bd2ec9150720bf
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-30 12:47:44 -0300

    Reads a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+This file contains a function
+"""
+
+
 def read_file(filename=""):
+    """
+    This function reads a text file
+    """
     with open(filename, 'r') as f:
         contenido = f.read()
         print(contenido, end="")
