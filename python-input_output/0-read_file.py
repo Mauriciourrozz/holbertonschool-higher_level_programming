d13bfec09a6475e9d00cc85470d065a92a80a95a
Mauriciourrozz <9366@holbertonstudents.com>
2024-09-30 12:51:13 -0300

    Reads a text file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ def read_file(filename=""):
     """
     This function reads a text file
     """
-    with open(filename, 'r') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         contenido = f.read()
         print(contenido, end="")
