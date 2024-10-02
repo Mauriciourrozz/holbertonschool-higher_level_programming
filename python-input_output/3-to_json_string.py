411eeb6bb0e202bf55356e0ae4ba297802ef4f01
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-02 12:15:17 -0300

    JSON

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This function contains a JSON module
+"""
 import json
+
+
 def to_json_string(my_obj):
+    """
+    This function return a json
+    """
     return json.dumps(my_obj)
