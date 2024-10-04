c568cef67a4c74947966ff176515c888ce737af4
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-04 14:23:00 +0000

    Return a dictionary description

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -9,10 +9,4 @@ def class_to_json(obj):
     """
     This function return a dictionary description with simple data structure
     """
-    vacio = {}
-    for objeto in obj.__dict__:
-        valor = obj.__dict__[objeto]
-        if isinstance(valor, (list, dict, str, int, bool)):
-            vacio[objeto] = valor
-
-    return vacio
+    return obj.__dict__
