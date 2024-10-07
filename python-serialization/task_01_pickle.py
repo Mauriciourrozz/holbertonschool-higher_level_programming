7ad4490817938d540e0dd480c817bcdccf044691
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-07 11:30:22 -0300

    Class CustomObject

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -2,7 +2,7 @@
 import pickle
 
 class CustomObject:
-    def __init__(self, name: str, age: int, is_student: bool):
+    def __init__(self, name, age, is_student):
         self.name = name
         self.age = age
         self.is_student = is_student
@@ -13,8 +13,11 @@ class CustomObject:
         print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
+        try:
             with open(filename, 'wb') as file:
                 return pickle.dump(self, file)
+
+        except EOFError:
             raise ("Ran out of input")
             return None
 
