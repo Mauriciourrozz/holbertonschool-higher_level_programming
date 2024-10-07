9ac4cc44bf3f1438415ef6dce967dd483cd1c4b1
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-07 11:25:45 -0300

    Class CustomObject

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import pickle
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
+
+    def serialize(self, filename):
+        try:
+            with open(filename, 'wb') as file:
+                return pickle.dump(self, file)
+
+        except EOFError:
+            raise ("File not found")
+            return None
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, 'rb') as file:
+            l = pickle.load(file)
+            return l
