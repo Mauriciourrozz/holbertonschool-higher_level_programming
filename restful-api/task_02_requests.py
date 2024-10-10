7425ae154ae03381142ebce264394c995b260c5b
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-10 11:55:59 -0300

    Requests

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,28 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    recuperar = requests.get('https://jsonplaceholder.typicode.com/posts')
+    print(f'Status Code: {recuperar.status_code}')
+
+    if recuperar.status_code == 200:
+        publicaciones = recuperar.json()
+        for i in publicaciones:
+            print(i['title'])
+
+def fetch_and_save_posts():
+    recuperar = requests.get('https://jsonplaceholder.typicode.com/posts')
+    if recuperar.status_code == 200:
+        publicacion = recuperar.json()
+        list_dic = []
+        for post in publicacion:
+            list_dic.append({
+                'id': post['id'],
+                'title': post['title'],
+                'body': post['body']
+            })
+
+    with open('posts.csv', 'x') as f:
+        write = csv.DictWriter(f, fieldnames=['id', 'title', 'body'])
+        write.writeheader()
+        write.writerows(publicacion)
