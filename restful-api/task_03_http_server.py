ab51a98278deb1a10b2e3fd51c84ea91a5318ece
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-11 11:28:57 -0300

    Http.server

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -34,7 +34,7 @@ class My_Handler(http.server.BaseHTTPRequestHandler):
             self.send_response(404)
             self.send_header("Content-type", "text/html")
             self.end_headers()
-            self.wfile.write("404 - Pagina no encontrada".encode('utf-8'))
+            self.wfile.write("404 Not Found".encode('utf-8'))
 
 with http.server.HTTPServer(("", PORT), My_Handler) as httpd:
     print("serving at port", PORT)
