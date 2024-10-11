b4cca6f3203022a32dec8a6c6deaf30e6f04fc43
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-11 17:01:00 -0300

    Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,32 @@
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+usuarios = {
+    "pepe": {"name": "Pepe", "age": 28, "city": "Montevideo"},
+    "fulano": {"name": "Fulano", "age": 30, "city": "Buenos aires"},
+    "mengano": {"name": "Mengano", "age": 25, "city": "Salto"},
+}
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+@app.route("/data", methods=["GET"])
+def data():
+    name_users = list(usuarios.keys())
+    return name_users
+
+@app.route("/status")
+def status():
+    return "OK"
+
+@app.route("/users/<username>", methods=['GET'])
+def devolver_segun_username(username):
+    usuario = usuarios.get(username)
+    if usuario:
+        return jsonify(usuario), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+if __name__ == "__main__":
+    app.run()
