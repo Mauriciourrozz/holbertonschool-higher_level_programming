a20e93208c8249b2ab2785fd914600c46790e624
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-14 11:52:09 -0300

    Flask

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -28,17 +28,17 @@ def devolver_segun_username(username):
     else:
         return jsonify({"error": "User not found"}), 404
 
-@app.route("/add_user", methods=['GET', 'POST'])
-def agregar_usuario():
-    data = request.get_json()
-    if 'username' not in data or not data['username']:
-        return jsonify({"error": "Username is required"}), 400
-    username = data['username']
+@app.route("/add_user", methods=['POST'])
+def add_user():
+    data = request.json
+    username = data.get("username")
     usuarios[username] = {
-    "name": data['name'],
-    "age": data['age'],
-    "city": data['city']
+        "name": data.get("name"),
+        "age": data.get("age"),
+        "city": data.get("city"),
     }
 
+    return jsonify({"message": "User added successfully", "user": usuarios[username]}), 201
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
