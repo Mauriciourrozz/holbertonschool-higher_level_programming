b06a5b83e9babbd14e8e3580d3d87ad2eb5c0d4a
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-16 21:16:03 -0300

    Basic security

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 
 app = Flask(__name__)
@@ -34,10 +34,47 @@ def login():
     data = request.get_json()
     username = data['username']
     password = data['password']
+
     if not usuarios or not check_password_hash(usuarios[username]['password'], password):
         return {"error": "access denied"}, 401
     token = create_access_token(identity={'username': username, 'role': usuarios[username]['role']})
     return {"access_token": token}, 200
+
+
+@app.route('/jwt-protected')
+@jwt_required()
+def jwt_protected():
+    return "JWT Auth: Access Granted", 200
+
+@app.route("/admin-only")
+@jwt_required()
+def admin_only():
+    traer_rol_usuario = get_jwt_identity()
+    rol = traer_rol_usuario['role']
+    if rol == 'admin':
+        return "Admin Access: Granted", 200
+    else:
+        return {"error": "Admin access required"}, 403
+
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    return jsonify({"error": "Missing or invalid token"}), 401
+
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    return jsonify({"error": "Invalid token"}), 401
+
+@jwt.expired_token_loader
+def handle_expired_token_error(err):
+    return jsonify({"error": "Token has expired"}), 401
+
+@jwt.revoked_token_loader
+def handle_revoked_token_error(err):
+    return jsonify({"error": "Token has been revoked"}), 401
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    return jsonify({"error": "Fresh token required"}), 401
 
 if __name__ == '__main__':
     app.run()
