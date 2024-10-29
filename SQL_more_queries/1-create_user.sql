89829465d2ddd7feecf4cdc4f5e6c3262f4660fe
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-29 11:31:43 -0300

    Create a user and have all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,7 @@
 -- Crea el usuario user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 
 GRANT ALL PRIVILEGES
 ON *.*
-TO 'user_0d_1'@'localhost'
-WITH GRANT OPTION;
+TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
