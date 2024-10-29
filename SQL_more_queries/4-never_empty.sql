c57cc9252dc0149bca91054abe847488573d05c1
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-29 19:59:17 -0300

    Create a table id_not_null

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,6 +1,6 @@
---Crea una tabla llamada id_not_null
+-- creates the table id_not_null on your MySQL server.
 
-CREATE TABLE IF NOT EXISTS id_not_null (
-id INT DEFAULT 1,
-name VARCHAR(256)
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT DEFAULT 1,
+    name VARCHAR(256)
 );
