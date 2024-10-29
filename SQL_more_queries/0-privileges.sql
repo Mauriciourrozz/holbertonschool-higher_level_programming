1766fb46535d9a25b7932976a4dbb2501f192826
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-29 10:48:58 -0300

    Show all privileges of user_0d_1 and user_0d_2

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
 -- Lista todos los priviegios de los usuarios user_0d_1 y user_0d_2
-SHOW GRANTS FOR user_0d_1@localhost AND user_0d_2@localhost;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
