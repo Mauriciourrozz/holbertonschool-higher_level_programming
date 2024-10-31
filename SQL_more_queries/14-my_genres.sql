52a5377c48c24cb8ed929c536d09930946ff8b75
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-31 15:51:45 -0300

    List all genres of the show Dexter

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 -- Importar el volcado de la base de datos desde hbtn_0d_tvshows
-SELECT tv_genres.name AS name
-FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_shows_id = tv_show_genres.show_id
-WHERE tv_shows.title = 'Dexter'
-ORDER BY tv_genres.name ASC;
+SELECT tg.name AS name
+FROM tv_genres tg
+JOIN tv_show_genres tsg ON tg.id = tsg.genre_id
+JOIN tv_shows ts ON ts.id = tsg.show_id
+WHERE ts.title = 'Dexter'
+ORDER BY tg.name ASC;
