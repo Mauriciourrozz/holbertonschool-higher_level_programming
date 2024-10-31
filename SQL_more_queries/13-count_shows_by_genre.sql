e2a0422a043c209d0b78123b5777ac8fe2fedd82
Mauriciourrozz <9366@holbertonstudents.com>
2024-10-31 12:20:21 -0300

    Select all genres

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -4,5 +4,5 @@ FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
 GROUP BY tv_genres.name
-HAVING number_of_show > 0
-ORDER BY number_of_show DESC;
+HAVING number_of_shows > 0
+ORDER BY number_of_shows DESC;
