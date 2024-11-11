e607ba9532ae398fa94a2f2ad9ddc21f4d869945
Mauriciourrozz <9366@holbertonstudents.com>
2024-11-11 14:00:45 -0300

    Create the render template for the files html

--- python-server_side_rendering/task_01_jinja.py
+++ python-server_side_rendering/task_01_jinja.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 def home():
    return render_template('index.html')
 
-@app.route('/')
+@app.route('/about')
 def about():
    return render_template('about.html')
 
-@app.route('/')
+@app.route('/contact')
 def contact():
    return render_template('contact.html')
 
